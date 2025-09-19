c6d086af255a1e8631a2a933b15baf4b6eb348a8
gaojr <gaojr47@hotmail.com>
2025-09-19 18:24:33 +0800

    feat: adapt the escaped URL

--- background.js
+++ background.js
@@ -2,16 +2,17 @@
  * 从Chrome网上应用店URL中提取扩展ID
  */
 const extractExtensionId = (url) => {
+  url = decodeURIComponent(url) // url转义
   return url
     ?.split('/detail/')[1] // 移除detail前的部分
     ?.split('?')[0] // 移除参数
     ?.replace(/\/$/, '') // 移除末尾“/”
     ?.replace(/\/reviews$/, '') // 移除末尾“/reviews”
     ?.replace(/.*\//, '') // 只保留最后一个“/”后的部分
-    ?.match(/^[a-zA-Z]{32}$/)?.[0]; // 校验扩展ID
-};
+    ?.match(/^[a-zA-Z]{32}$/)?.[0] // 校验扩展ID
+}
 
-const menuId = 'redirect-to-crx4chrome';
+const menuId = 'redirect-to-crx4chrome'
 
 /**
  * 创建右键菜单项
@@ -22,21 +23,26 @@ chrome.runtime.onInstalled.addListener(() => {
     title: '跳转crx4chrome',
     contexts: ['link'],
     documentUrlPatterns: ['<all_urls>'],
-    targetUrlPatterns: ['*://chrome.google.com/webstore/detail/*', '*://chromewebstore.google.com/detail/*'],
-  });
-});
+    targetUrlPatterns: [
+      '*://chrome.google.com/webstore/detail/*',
+      '*://chromewebstore.google.com/detail/*',
+      '*://*/*%2Fchrome.google.com%2Fwebstore%2Fdetail%2F*',
+      '*://*/*%2Fchromewebstore.google.com%2Fdetail%2F*',
+    ],
+  })
+})
 
 /**
  * 处理右键菜单点击事件
  */
 chrome.contextMenus.onClicked.addListener((info, _tab) => {
   if (info.menuItemId === menuId) {
-    const extensionId = extractExtensionId(info.linkUrl);
+    const extensionId = extractExtensionId(info.linkUrl)
     if (extensionId) {
       // 成功提取到扩展ID，进行重定向
-      const redirectUrl = `https://www.crx4chrome.com/extensions/${extensionId}/`;
+      const redirectUrl = `https://www.crx4chrome.com/extensions/${extensionId}/`
       // 在新标签页中打开
-      chrome.tabs.create({ url: redirectUrl });
+      chrome.tabs.create({ url: redirectUrl })
     }
   }
-});
+})
