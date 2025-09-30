2ba811ad80a5e1670c1a4570e799f099ce75c1e0
gaojr <gaojr47@hotmail.com>
2025-09-30 20:00:06 +0800

    目录列表从固定常量改为变量

--- src/background.js
+++ src/background.js
@@ -1,10 +1,8 @@
-/**
- * 目录常量
- */
-const MENUS = {
-  "cws-redirector-crx4chrome": { name: "跳转crx4chrome", url: "https://www.crx4chrome.com/extensions" },
-  "cws-redirecto-crxsosor": { name: "跳转crx搜搜", url: "https://www.crxsoso.com/webstore/detail" },
-  "cws-redirecto-crxdl": { name: "跳转crxdl", url: "https://crxdl.com/detail/g" },
+let menus = {};
+const DEFAULT_MENUS = {
+  crx4chrome: { name: "跳转crx4chrome", url: "https://www.crx4chrome.com/extensions" },
+  crxsosor: { name: "跳转crx搜搜", url: "https://www.crxsoso.com/webstore/detail" },
+  crxdl: { name: "跳转crxdl", url: "https://crxdl.com/detail/g" },
 };
 const CREATE_PROPERTIES = {
   contexts: ["link"],
@@ -19,6 +17,7 @@ const CREATE_PROPERTIES = {
     "*://*/*%2Fchromewebstore.google.com%2Fdetail%2F*",
   ],
 };
+const STORAGE_KEY = "cws-redirector";
 
 /**
  * 从Chrome网上应用店链接中提取信息
@@ -29,7 +28,7 @@ const extractUrl = (url) => {
     ?.split("?")[0] // 移除参数
     ?.replace(/\/$/, "") // 移除末尾“/”
     ?.replace(/\/reviews$/, ""); // 移除末尾“/reviews”
-  let [extensionName, extensionId] = cleanUrl.split("/");
+  let [extensionName, extensionId] = cleanUrl?.split("/") || [];
   // 校验扩展ID
   extensionId = extensionId?.match(/^[a-zA-Z]{32}$/)?.[0];
   return { extensionName, extensionId };
@@ -41,7 +40,7 @@ const extractUrl = (url) => {
 chrome.runtime.onInstalled.addListener(() => {
   for (const [k, v] of Object.entries(MENUS)) {
     chrome.contextMenus.create({
-      id: k,
+      id: `${STORAGE_KEY}-${k}`,
       title: v.name,
       ...CREATE_PROPERTIES,
     });
@@ -52,7 +51,7 @@ chrome.runtime.onInstalled.addListener(() => {
  * 处理右键菜单点击事件
  */
 chrome.contextMenus.onClicked.addListener((info, _tab) => {
-  const menu = MENUS[info.menuItemId];
+  const menu = menus[info.menuItemId];
   if (menu) {
     const { extensionId } = extractUrl(info.linkUrl);
     if (extensionId) {
